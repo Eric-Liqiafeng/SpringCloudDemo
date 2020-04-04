cf1af029ff99801c1e604010684175a6fee60bba
Eric Li <1127883584@qq.com>
2020-04-04 17:21:25 +0800

    add configureMessageConverters

---
+++ eric-ad-service/ad-common/src/main/java/com/eric/ad/conf/WebConfiguration.java
@@ -0,0 +1,22 @@
+package com.eric.ad.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * @author Eric Li
+ * @date 2020/4/4 5:17 下午
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.clear();
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+}
