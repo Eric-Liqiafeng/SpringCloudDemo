7f5d45f2f94ac3f842b9310e425d3eabb5192f35
Eric Li <1127883584@qq.com>
2020-04-06 11:10:17 +0800

    add unit_condition

---
+++ eric-ad-service/ad-sponsor/src/main/java/com/eric/ad/entity/unit_condition/AdUnitDistrict.java
@@ -0,0 +1,48 @@
+package com.eric.ad.entity.unit_condition;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Eric Li
+ * @date 2020/4/5 3:13 下午
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ad_unit_district")
+public class AdUnitDistrict {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Basic
+    @Column(name = "unit_id", nullable = false)
+    private Long unitId;
+
+    @Basic
+    @Column(name = "province", nullable = false)
+    private String province;
+
+    @Basic
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    public AdUnitDistrict(Long unitId, String province, String city) {
+        this.unitId = unitId;
+        this.province = province;
+        this.city = city;
+    }
+}
---
+++ eric-ad-service/ad-sponsor/src/main/java/com/eric/ad/entity/unit_condition/AdUnitKeyword.java
@@ -0,0 +1,37 @@
+package com.eric.ad.entity.unit_condition;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * @author Eric Li
+ * @date 2020/4/5 3:07 下午
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ad_unit_keyword")
+public class AdUnitKeyword {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Basic
+    @Column(name = "unit_id", nullable = false)
+    private Long unitId;
+
+    @Basic
+    @Column(name = "keyword", nullable = false)
+    private String keyword;
+
+    public AdUnitKeyword(Long unitId, String keyword) {
+        this.unitId = unitId;
+        this.keyword = keyword;
+    }
+}
